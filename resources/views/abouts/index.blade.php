f92ba3ab94a7950517e0154c0bedf018b9b31585
Oakkar Pyae Phyo <41377931+OakkarPyaePhyo@users.noreply.github.com>
2019-10-11 19:43:17 +0630

    update

--- resources/views/abouts/index.blade.php
+++ resources/views/abouts/index.blade.php
@@ -9,10 +9,8 @@
     <div class="container">
         <div class="row" style="background-color:white;">
             <div class="col-md-12 col-sm-12">
-                <b> <font color="#7CB9E8" size="6"> About Us: </font> </b>
-                <font size="4">
-                    Text
-                </font>
+                <b> <font color="#7CB9E8" size="6"> About Us </font> </b>
+                
             </div>
         </div>
         <hr>
@@ -27,10 +25,25 @@
         <hr>
         <div class="row" style="background-color:white;">
             <div class="col-md-12 col-sm-12">
-                    <b> <font color="#7CB9E8" size="6"> Our Mission: </font> </b>
-                <font size="4">
+
+            <font size="4"> <br>
+                &emsp; The Environmentalist Project brings environmental clubs to Myanmar in order to build awareness of environmental topics, explore present problems of pollution, and develop ways in which the community can address these problems. This generation will someday be the one in charge, so in the future, people will be taking their knowledge about the natural world into account when they make important decisions. We will create volunteers camps to clean around the country. We first start as small where we go around significant places in Yangon. We also have shopping site as a side job where we sell ecofriendly product online. This project is entirely run by volunteers.
+            </font> <br>
+                <b> <font color="#7CB9E8" size="6"> Our Mission: </font> </b>
+                <font size="4"> <br>
                     Environmentalist want to create a media between events and volunteers. Also, we sell the environmental staffs with cheaper price to protect our beautiful earth. 
-                </font>
+                </font> <br>
+
+                
+                <br> <b> <font color="#7CB9E8" size="6"> What We Believe </font> </b> <br>
+                <font size="4">
+                <ul>
+                <li> The environment is in danger and we need to take action to save it. </li>
+                <li> Many people lack an understanding of the environment, and if they do have knowledge they don’t know how they can help. </li>
+                <li> The people who learnt about environment will develop their understanding of the problems facing the natural world and give them the opportunity to make real change. </li>
+                <li> They will apply their knowledge and expertise about the environment to important decisions and make a difference in the world. </li>
+                </ul>
+            </font>
             </div>
         </div>
         <hr>
